4fee4fb702b2e3f4bea2873fd47eec883820982f
Learning Heroku <learning.heroku@gmail.com>
2011-08-23 20:59:40 +0100

    Blog app - v0.4

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :body, :presence => true
+  belongs_to :user 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '1d06feee3b834e5a311913f332d13c889fc8bb0810e0afe6920cd9671897c3dfdce072e5c86e3fde89ae65fa9a9a9ed3ae7830adff18ce3fcba5a0a29fe93d1e'
